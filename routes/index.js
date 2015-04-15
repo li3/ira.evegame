7a29dead4275d4ce6618972273b23aed55b9ae54
li3 <kylelydic@gmail.com>
2015-04-14 21:02:17 -0500

    Testing jenkins environment variable settings

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var dbconnection = process.env.APP_MONGODB_CONNECTION || "no-db-connection";
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {});
+  res.render('index', { dbconnection: dbconnection});
 });
 
 module.exports = router;
